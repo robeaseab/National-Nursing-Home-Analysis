ea62ecf47716f7a35e1484d77ae7ad72d9cf54f9
searobea <searobea@gmail.com>
2019-11-14 13:00:37 -0600

    csv and ipynb for ML analysis was added

--- US_SNF_Analysis.sql
+++ US_SNF_Analysis.sql
@@ -20,7 +20,7 @@ select count(*) from provider_info
 where `Provider City` like "%Chicago%"
 and  `Provider State` like ("IL");
 #count of Chicago Providers = 76.  Wildcards used because here is some variation in city input like "south chicago" etc.  IL filter pulled out one IN entry which would be there otherwise. 
-select `Ownership Type`, count(*)
+select `Ownership Type`, `Provider Name`, count(*)
 from provider_info
 where `Provider City` like "%Chicago%"
 and  `Provider State` like ("IL")
@@ -54,14 +54,41 @@ select avg(`Number of Certified Beds`)
 from provider_info;
 #national average: 111.4079 beds
 
-
-
-
 #Inspect dataset, get number of rows/columns
 SELECT `COLUMN_NAME`
 FROM `INFORMATION_SCHEMA`.`COLUMNS` 
 WHERE `TABLE_SCHEMA`='snf' 
-    AND `TABLE_NAME`='provider_info';
+    AND `TABLE_NAME`='deficiencies_displayed_on_nursing_home_compare___cycle_1';
+    
+alter table deficiencies_displayed_on_nursing_home_compare___cycle_1 rename to deficiencies;
+#Inspect dataset, get number of rows/columns
+SELECT `COLUMN_NAME`
+FROM `INFORMATION_SCHEMA`.`COLUMNS` 
+WHERE `TABLE_SCHEMA`='snf' 
+    AND `TABLE_NAME`='deficiencies';
+    
+select `Provider Name`, count(`Deficiency Tag Number`)
+from deficiencies
+group by `Provider Name`
+order by count(`Deficiency Tag Number`) desc;
+#top result country: MILLER'S MERRY MANOR	144
+select `Provider Name`, count(`Deficiency Tag Number`)
+from deficiencies
+where `Provider State` in ("IL")
+group by `Provider Name`
+order by count(`Deficiency Tag Number`) desc;
+#top result IL: APERION CARE CAPITOL	46 and GENERATIONS AT LINCOLN	46
+
+#`Provider Name`, `Overall Rating`
+select * from provider_info
+where `Provider City` like "%Chicago%"
+and  `Provider State` like ("IL")
+and `Overall Rating` in (1)
+order by `Provider Name`;
+
+
+
+
 
 
     
